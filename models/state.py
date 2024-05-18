add07de2083f1185b21b3f31293525a60bfa16a9
nabil mkadmi <nabilmkadmi@gmail.com>
2024-05-18 16:14:02 -0700

    m

--- models/state.py
+++ models/state.py
@@ -1,13 +1,35 @@
 #!/usr/bin/python3
-"""Defines the State class."""
-from models.base_model import BaseModel
+"""This is the state class"""
+import os
+import models
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Integer
+from sqlalchemy.orm import relationship
 
 
-class State(BaseModel):
-    """Represent a state.
-
+class State(BaseModel, Base):
+    """This is the class for State
     Attributes:
-        name (str): The name of the state.
+        name: input name
+        cities = relationship between state and city tables.
     """
 
-    name = ""
+    __tablename__ = 'states'
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        name = Column(String(128), nullable=False)
+        cities = relationship(
+            'City', back_populates='state',
+            cascade='all, delete, delete-orphan')
+
+    else:
+        name = ""
+
+        @property
+        def cities(self):
+            """returns list of Cities and some relationships"""
+            cities_instances = []
+            cities_dict = models.storage.all(models.City)
+            for key, value in cities_dict.items():
+                if self.id == value.state_id:
+                    cities_instances.append(value)
+            return cities_instances
