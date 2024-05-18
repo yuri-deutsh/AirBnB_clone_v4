41d7777cee41c95f2db94b0476bee3101e14e02d
nabil mkadmi <nabilmkadmi@gmail.com>
2024-05-18 16:15:49 -0700

    m

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,20 @@
 #!/usr/bin/python3
-"""__init__ magic method for models directory"""
-from models.engine.file_storage import FileStorage
+"""create a unique FileStorage instance for your application"""
+import os
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 
-storage = FileStorage()
-storage.reload()
+if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
+    storage.reload()
+else:
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
+    storage.reload()
